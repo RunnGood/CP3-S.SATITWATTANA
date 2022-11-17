e74a76bca344f41b8f9e18f8bcfba980834f7569
RunnGood <56duckstudio@gmail.com>
2022-11-17 19:54:35 +0700

    1

---
+++ Exercise4_Sitporn.S.py
@@ -0,0 +1,10 @@
+score1=0.0
+score2=0.0
+score3=0.0
+score4=0.0
+
+print("--- Your Score ---",)
+print("Foundation English   : ",score1)
+print("General Business     : ",score2)
+print("Introduction to Computer Systems : ",score3)
+print("Computer Programming : ",score4)
